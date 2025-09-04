813c1a4a670bb7cecc404f6ca5ae9349d7d1f914
Gustavo Silva <gustavo.silva.nav@gmail.com>
2025-09-03 21:53:32 -0300

    🔥 Removing some comments

--- src/server/server.go
+++ src/server/server.go
@@ -10,17 +10,13 @@ import (
 )
 
 func StartServer(cfg *config.Config) {
-	// This is part of the standard library and avoids external dependencies.
+	// new server
 	mux := http.NewServeMux()
 
-	// Call the function from the routes package to set up all the endpoints.
+	// Routes
 	routes.SetupRoutes(mux)
 
-	// Log the server start message.
 	log.Printf("🚀 Starting server on %d port", cfg.PORT)
-
-	// Start the server and listen for incoming requests.
-	// The server uses the ServeMux to handle requests based on the registered routes.
 	err := http.ListenAndServe(fmt.Sprintf(":%d", cfg.PORT), mux)
 	if err != nil {
 		log.Fatalf("Server failed to start: %v", err)
