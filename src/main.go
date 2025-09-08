273eb97c6c78350ec609e476fb95bad0559a6ca9
Gustavo Silva <gustavo.silva.nav@gmail.com>
2025-09-08 10:46:53 -0300

    ♻️ Refactoring server entrypoint
    to have a graceful shutdown in case of failures

--- src/main.go
+++ src/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gsn_manager_service/src/adapters"
 	"github.com/gsn_manager_service/src/config"
@@ -22,5 +27,42 @@ func main() {
 
 	connections.CreateAllFactories(result.Db)
 
-	server.StartServer(config)
+	// TODO: for now this root context is not being used, so is not doing anything
+	// I could remove it, but it would be used later
+	_, cancel := context.WithCancel(context.Background()) // Create root ctx
+	defer cancel()
+
+	srv := server.StartServer(config)
+	serverErr := make(chan error, 1)
+	go func() {
+		adapters.Logger.Info().Msg(fmt.Sprintf("🚀 Starting server on %d port", config.PORT))
+		if err := srv.ListenAndServe(); err != nil {
+			serverErr <- err
+		}
+	}()
+
+	// Signal listener goroutine
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	select {
+	case sig := <-quit:
+		adapters.Logger.Info().Msgf("📴 Received shutdown signal: %s", sig)
+		cancel()
+	case err := <-serverErr:
+		adapters.Logger.Error().Err(err).Msg("💥 Server crashed")
+		cancel()
+	}
+
+	// Give some time for cleanup before force exit
+	shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer shutdownCancel()
+
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		adapters.Logger.Error().Err(err).Msg("⚠️ Server forced to shutdown")
+	} else {
+		adapters.Logger.Info().Msg("✅ Server stopped gracefully")
+	}
+
+	adapters.Logger.Info().Msg("👋 Cleanup complete, exiting.")
 }
--- src/server/server.go
+++ src/server/server.go
@@ -3,25 +3,30 @@ package server
 import (
 	"fmt"
 	"net/http"
-	"os"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/gsn_manager_service/src/adapters"
 	"github.com/gsn_manager_service/src/config"
 	"github.com/gsn_manager_service/src/server/middlewares"
 	"github.com/gsn_manager_service/src/server/routes"
 )
 
-func StartServer(cfg *config.Config) {
+func StartServer(cfg *config.Config) *http.Server {
 	r := chi.NewRouter()
 	middlewares.SetupMiddleware(r)
 
 	routes.SetupRoutes(r)
 
-	adapters.Logger.Info().Msg(fmt.Sprintf("🚀 Starting server on %d port", cfg.PORT))
-	err := http.ListenAndServe(fmt.Sprintf(":%d", cfg.PORT), r)
-	if err != nil {
-		adapters.Logger.Error().Msg(fmt.Sprintf("Server failed to start: %v", err))
-		os.Exit(1)
+	// err := http.ListenAndServe(fmt.Sprintf(":%d", cfg.PORT), r)
+	// if err != nil {
+	// 	adapters.Logger.Error().Msg(fmt.Sprintf("Server failed to start: %v", err))
+	// 	return err
+	// }
+	// return nil
+
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%d", cfg.PORT),
+		Handler: r,
 	}
+
+	return server
 }
